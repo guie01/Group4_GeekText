224c7ac17613e90d3d1c67caf4e5676ff736b7b7
SorenErvik <ervik.soren@gmail.com>
2024-04-01 19:06:21 -0500

    Stuff

--- src/main/java/com/geektext/bookComments/BookCommentsDAO.java
+++ src/main/java/com/geektext/bookComments/BookCommentsDAO.java
@@ -1,3 +1,22 @@
+/*package com.geektext.bookComments;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigInteger;
+import java.util.List;
+import com.geektext.bookComments.BookCommentsRepository;
+
+public interface BookCommentsDAO {
+
+    void saveComment(BookComments bookComment);
+
+    default List<BookComments> getCommentsByBookIsbn(BigInteger bookIsbn) {
+        return bookCommentsRepository.findCommentsByBookIsbn(bookIsbn);
+    }
+
+    void setBookCommentsRepository(BookCommentsRepository bookCommentsRepository);
+} */
+
 package com.geektext.bookComments;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/geektext/bookComments/BookCommentsRepository.java
+++ src/main/java/com/geektext/bookComments/BookCommentsRepository.java
@@ -1,3 +1,18 @@
+/*package com.geektext.bookComments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public interface BookCommentsRepository extends JpaRepository<BookComments, Integer> {
+
+    static List<BookComments> findCommentsByBookIsbn(BigInteger isbn) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findCommentsByBookIsbn'");
+    }
+}*/
+
 package com.geektext.bookComments;
 
 import org.springframework.data.jpa.repository.JpaRepository;
